0f2333e6469901d9346f2c7bc108ae45d76a70d9
Nikia Gubenkov <n.gubenkov@gmail.com>
2019-08-03 19:43:57 +0300

    optimization has been done(using xpath instead of previous)

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ https://medium.com/ymedialabs-innovation/web-scraping-using-beautiful-soup-and-s
 selenium + beautifulsoup because it dynamic page
 '''
 def browser_setup():
+    '''
+    setup browser
+    '''
     chromeOptions = webdriver.ChromeOptions()
     prefs = {"download.default_directory": "/Users/frozmannik/Desktop/TEXAS"}
     chromeOptions.add_experimental_option("prefs", prefs)
@@ -22,26 +25,35 @@ def browser_setup():
     # browser = webdriver.Chrome('C:\\Users\Frozm\PycharmProjects\\biologyScrapeData\\files\win\chromedriver.exe')
     return browser
 
-
-
-def duno():
-    '''dont need this code'''
-    url = "http://wwwgisp.rrc.texas.gov/GISViewer2/"
-    crawl_url = get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    crawl_url.raise_for_status()
-    soup = BeautifulSoup(crawl_url.text)
-    print(soup)
-
 def open_page(url):
+    '''
+    open browser and click
+    '''
     browser = browser_setup()
     browser.get(url)
     try:
-        buttons = WebDriverWait(browser, 20).until(EC.presence_of_all_elements_located((By.ID, "rrcsearchButton")))
-        print(buttons)
-        buttons[0].click() # opens search
+        btn_Magnifying_Glass = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="rrcsearchButton"]')))
+        #print(btn_Magnifying_Glass)
+        btn_Magnifying_Glass.click() # opens search
+
+        btn_Survey = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="dijit_rrcGisAnchorMenuItem_7_text"]')))
+        btn_Survey.click()
+        inputData(browser)
         input()
     except Exception as e:
         print(e)
+
+
+def inputData(browser,country=None, block=None, section=None):
+    ''' [ dijitReset ] is drop down element contains all counties
+      path should be somethin like :   [ dijitReset ] / [dijitReset dijitMenuItem] / [dijitReset dijitMenuItemLabel] [ inner HTML ]
+    '''
+    select_countries = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="countySelect"]/tbody/tr/td[2]/div[1]')))
+    select_countries.click()
+    items = WebDriverWait(browser, 20).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'dijitMenuItemLabel'))) # list of drop down items
+    for _ in items:
+        print(_.get_attribute('innerHTML'))
+
 
 
 if __name__ == '__main__':
